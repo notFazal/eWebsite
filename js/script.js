7b34c8826960963268e856f61b3df94df96ac78c
satyabh <satyabh2003@gmail.com>
2024-06-16 21:30:00 -0700

    reorganized header bar for mobile support
    added "hamburger" that toggles according to viewframe

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ document.getElementById('contact-link').addEventListener('click', function(event
     window.location.href = 'contact.html';
 });
 
-
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
